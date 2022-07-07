ea07b95b98240786ffee2bd38ad9ef6a8dee9b42
FrederickBizzardo <nyembofred@gmail.com>
2022-07-07 12:57:41 +0200

    message

--- flavorsapi.py
+++ flavorsapi.py
@@ -376,7 +376,7 @@ def get_recipes(rep_name):
     title = request.json['title']
     recipes = [i[0] for i in cur.fetchall() if title['title'] in rep_name]
 
-    return jsonify({'recipe': recipes[0]}) #can change array position from 0 - 4 
+    return jsonify({'recipe': recipes}) #can change array position from 0 - 4 
     #original return jsonify({'recipe': recipe})
 
 # Connect to database
