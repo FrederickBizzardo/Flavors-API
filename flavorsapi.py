ea38abec7c2c9274dbb38f39cf57c80c44b0600f
FrederickBizzardo <nyembofred@gmail.com>
2022-07-18 22:25:09 +0200

    commit

--- flavorsapi.py
+++ flavorsapi.py
@@ -194,18 +194,49 @@ app = Flask(__name__)
 try: 
     #url = 'https://www.recipe-free.com/recipes/easy-swedish-meatballs---jamie-oliver-recipe/129381'
     #url = 'https://www.recipe-free.com/recipes/best-african-vegetarian-stew/129364'
-    url = 'https://www.recipe-free.com/recipes/real-coconut-chicken-tenders-recipe-joel-robuchon-recipe/129450'
-    result = requests.get(url).text
+    #url = 'https://www.recipe-free.com/recipes/real-coconut-chicken-tenders-recipe-joel-robuchon-recipe/129450'
+    #result = requests.get(url).text
 
-    doc = BeautifulSoup(result, 'html.parser')
-    rtitle = doc.find('h1', {'class': 'red'}).text.strip()
-    ringredients = doc.find('div', {'class': 'col-md-12 for-padding-col'}).find_all('p')[0].text.strip()
-    rservings = doc.find('div', {'class': 'times'}).findAll('div', {'class': 'times_tab'})[1].findAll('div', {'class': 'f12 f12'})[1].text.strip()
-    rinstructions = doc.find('div', {'class': 'col-md-12 for-padding-col'}).find_all('p')[1].text.strip()
-    print(rtitle)
-    print(ringredients)
-    print(rservings)
-    print(rinstructions)
+    page = 1
+    url_no = 1
+    titles = []
+    links = []
+    links_dict = {}
+    #url_link = 1
+    while page != 7:
+        url = f"https://www.recipe-free.com/categories/meat-recipes/{page}"
+        #print(url)
+        response = requests.get(url)
+        html = response.content
+        soup = BeautifulSoup(html, "lxml")
+        for a in soup.find('div', {'class': 'category_content centerindent for-this'}).findAll('a', {'class': 'day'}):
+            titles.append(a.get_text(strip=True))
+            links.append(a.get('href'))
+            #print(titles)
+        page = page + 1
+    for title in titles[:80]:
+        print(title)
+    for i, link in enumerate(links[:120], url_no):
+        #print(f'url {url_no}: {link}')
+        name = f'url {url_no}'
+        links_dict = {name : link}
+        #links_dict = {f'url {url_no}': link}
+        url_no += 1
+        #for dict in links_dict:
+        #print(links_dict)
+        print(links_dict[name])
+        #code to get info from each link stored in links_dict
+        url_grapper = links_dict[name]
+        result = requests.get(url_grapper).text    
+        doc = BeautifulSoup(result, 'html.parser')
+        rtitle = doc.find('h1', {'class': 'red'}).text.strip()
+        ringredients = doc.find('div', {'class': 'col-md-12 for-padding-col'}).find_all('p')[0].text.strip()
+        rservings = doc.find('div', {'class': 'times'}).findAll('div', {'class': 'times_tab'})[1].findAll('div', {'class': 'f12 f12'})[1].text.strip()
+        rinstructions = doc.find('div', {'class': 'col-md-12 for-padding-col'}).find_all('p')[1].text.strip()
+        print(rtitle)
+        print(ringredients)
+        print(rservings)
+        print(rinstructions)
 except:
     print("Failed to establish a new connection")
 
