a1173aa414c2eec09bb40e93567aff24a5fb3d52
FrederickBizzardo <nyembofred@gmail.com>
2024-06-28 16:24:38 +0200

    initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 from api.flavorsapi import app
         
     
 if __name__ == "__main__":
     app.run()
+=======
+from flavorsapi import app
+
+if __name__ == "__main__":
+    app.run()
+
+>>>>>>> 09c58a23 (Initial commit)
